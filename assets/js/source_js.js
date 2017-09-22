e9f5c11f20e4c054eb613d36e7fb5328c5b56247
musfiqurcse <md.musfiqurcse@outlook.com>
2017-09-22 20:22:22 +0600

    Promise completed

--- assets/js/source_js.js
+++ assets/js/source_js.js
@@ -73,3 +73,108 @@ ${
 `
 document.getElementById('template').innerHTML = extra
 
+//string feature
+
+let theString = 'Hello I love Javascript'
+
+
+//startswith()
+//endswith()
+//includes()
+
+console.log(theString.startsWith('Hello'))
+
+console.log(theString.endsWith('Hello'))
+
+let args = [1,2,2,3]
+let args1 =[3,4,5,6]
+
+function greet($greeting= 'Hello World'){
+    console.log($greeting)
+    console.log(args+','+args1)
+}
+
+
+greet(...args,...args1)
+
+greet(...args,...args1)
+/*
+
+
+*/
+
+
+let carWeakSet = new WeakSet()
+let car1 = {
+    make: 'Honda',
+    model: 'Civic'
+    
+}
+
+let car2 = {
+    make: 'Cara',
+    model: 'Cic'
+    
+}
+console.log(carWeakSet)
+carWeakSet.add(car1)
+carWeakSet.add(car2)
+console.log(carWeakSet)
+carWeakSet.delete(car1)
+console.log(carWeakSet)
+
+function Prefixer(prefix){
+    this.prefix = prefix 
+}
+Prefixer.prototype.prefixArray  = function(arr){
+    let self  = this;
+    return arr.map(function(x){
+
+        console.log(self.prefix + x)
+    });
+}
+let pre  = new Prefixer('Hello')
+pre.prefixArray(['Shubho', ' Saha'])
+
+
+//Promise
+
+var myPromise = new Promise(function(resolve,reject){
+    setTimeout(() => resolve(4), 2000)
+})
+
+myPromise.then((res) => {
+    res += 3
+    console.log(res)
+})
+
+function getData(method,url){
+    return new Promise(function(resolve,reject){
+        let xhr = new XMLHttpRequest()
+        xhr.open(method,url)
+        xhr.onload = function(){
+            if(this.status >= 200 && this.status < 300){
+                resolve(xhr.response);
+            }else{
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                })
+            }
+        };
+        xhr.onerror = function(){
+            reject({
+                status: this.status,
+                statusText: xhr.statusTExt
+            })
+        };
+        xhr.send();
+    })
+}
+
+getData('GET','http://jsonplaceholder.typicode.com/todos').then((data) => {
+    console.table(data)
+    console.log('testing')
+}).catch(function(err){
+  console.log(err)  
+});
